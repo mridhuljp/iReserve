297f7abd18ab338c23f3a27f140167147780848b
mridhuljp <mpax@cisco.com>
2015-06-25 21:15:20 -0400

    Initial Commit

---
+++ Main.py
@@ -0,0 +1,13 @@
+class Reserve(object):
+    def __init__(self):
+        pass
+
+
+    def HomeMenu(self):
+        print "  !!!!!!!!!  Welcome to iReserve !!!!!!!!!!!!!"
+        print " 1. Login : "
+        print " 2. Signup : "
+
+
+kickoff=Reserve()
+kickoff.HomeMenu()
