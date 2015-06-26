ea4b9af40f80973f5c98459cd8ba6d19c6a7f4b2
mridhuljp <mpax@cisco.com>
2015-06-25 21:28:07 -0400

    Updates

--- Main.py
+++ Main.py
@@ -2,12 +2,30 @@ class Reserve(object):
     def __init__(self):
         pass
 
+    def iLogin(self):
+        pass
+
+    def iSignup(self):
+        pass
 
     def HomeMenu(self):
         print "  !!!!!!!!!  Welcome to iReserve !!!!!!!!!!!!!"
         print " 1. Login : "
         print " 2. Signup : "
+        print " ----------- "
+        menuChoice=str(raw_input("Enter the input :"))
+        if menuChoice == "1":
+            self.iLogin()
+        elif menuChoice == "2":
+            self.iSignup()
+        else:
+            print "Option Invalid !!!!"
+            self.HomeMenu()
 
 
+
+
+
+####### Objects #########
 kickoff=Reserve()
 kickoff.HomeMenu()
