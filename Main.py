7e469cbf9d5af9b4d28ebd8e5144ac09f0e64e71
mridhuljp <mpax@cisco.com>
2015-06-28 18:37:19 -0400

    Cancellation_ Audited

--- Main.py
+++ Main.py
@@ -1,13 +1,12 @@
 class Reserve(object):
-    UsersTable={}
-    srv1 =[]
-    srv1.extend(range(1,21))
-    UserSession=False
-    iBooked=0
+    UsersTable = {}
+    srv1 = []
+    srv1.extend(range(1, 21))
+    UserSession = False
+    iBooked = 0
+
     def __init__(self):
-        self.line="=" * 25
-
-
+        self.line = "=" * 25
 
     def UserExists(self):
         if self.Des_username in self.UsersTable.keys():
@@ -17,17 +16,17 @@ class Reserve(object):
 
     def iLogin(self):
         print self.line
-        self.Des_username=raw_input("Username : ")
-        self.ifUserExists=self.UserExists()
-        if self.ifUserExists >0:
+        self.Des_username = raw_input("Username : ")
+        self.ifUserExists = self.UserExists()
+        if self.ifUserExists > 0:
             print "Sorry !! Invalid User"
             print self.line
             self.iLogin()
         else:
-            self.Des_password=raw_input("Password : ")
+            self.Des_password = raw_input("Password : ")
             if self.UsersTable[self.Des_username] == self.Des_password:
-                print " User %s Successfully Logged in !!" %(self.Des_username)
-                self.UserSession=True
+                print " User %s Successfully Logged in !!" % (self.Des_username)
+                self.UserSession = True
                 print self.line
                 self.HomeMenu()
             else:
@@ -35,9 +34,8 @@ class Reserve(object):
                 print self.line
                 self.iLogin()
 
-
     def ValidatePass(self):
-        if len(self.Des_password) <=3:
+        if len(self.Des_password) <= 3:
             return 1
         else:
             return 0
@@ -46,23 +44,23 @@ class Reserve(object):
         print self.UsersTable
 
     def UserAdd(self):
-        self.UsersTable[self.Des_username]=self.Des_password
+        self.UsersTable[self.Des_username] = self.Des_password
         print "User Added Successfully !!!"
         print self.line
-        #self.PrintUsers()
+        # self.PrintUsers()
         print self.line
         self.HomeMenu()
 
     def iSignup(self):
         print self.line
-        self.Des_username=raw_input("Enter desired username : ")
-        self.iUserReturn=self.UserExists()
+        self.Des_username = raw_input("Enter desired username : ")
+        self.iUserReturn = self.UserExists()
         if self.iUserReturn == 0:
             print "User Exists !!!! "
             self.iSignup()
-        self.Des_password=raw_input("Enter your password [Minimum 4 chars] : ")
-        self.ValidPassword=self.ValidatePass()
-        if self.ValidPassword ==1:
+        self.Des_password = raw_input("Enter your password [Minimum 4 chars] : ")
+        self.ValidPassword = self.ValidatePass()
+        if self.ValidPassword == 1:
             print "Sorry !!! Password Didn't match the requirements !!"
             self.iSignup()
         else:
@@ -75,24 +73,20 @@ class Reserve(object):
         print " 3. Reserve : "
         print " 4. Exit! : "
         print " ----------- "
-        menuChoice=str(raw_input("Enter the input :"))
+        menuChoice = str(raw_input("Enter the input :"))
         if menuChoice == "1":
             self.iLogin()
         elif menuChoice == "2":
             self.iSignup()
-        elif menuChoice =="4":
+        elif menuChoice == "4":
             exit()
-        elif menuChoice=="3":
+        elif menuChoice == "3":
             self.iReserve()
         else:
             print "Option Invalid !!!!"
             self.HomeMenu()
 
-
     def iReserve(self):
-
-
-
         def iBook(self):
             print "Book !!!"
             print self.line
@@ -101,14 +95,14 @@ class Reserve(object):
                 print self.line
                 print "Legend : 'B' - Booked Seat . 'R' - Reserved Seat"
                 print self.line
-                self.userseat=raw_input("Enter your choice :")
-                self.iBooked=self.userseat
-                if int(self.userseat) > 20 or self.srv1[int(self.userseat)] =="B":
+                self.userseat = raw_input("Enter your choice :")
+                self.iBooked = self.userseat
+                if int(self.userseat) > 20 or self.srv1[int(self.userseat)] == "B":
                     print "Sorry !! Seat Already booked !!!"
                     print self.line
                     self.iReserve()
                 else:
-                    self.srv1[int(self.userseat) -1 ]=self.userseat.replace(self.userseat,"B")
+                    self.srv1[int(self.userseat) - 1] = self.userseat.replace(self.userseat, "B")
                 print "Seat Booked Successfully !!!"
                 print self.srv1
                 self.HomeMenu()
@@ -122,19 +116,19 @@ class Reserve(object):
             while (self.UserSession):
                 print self.srv1
                 print self.line
-                if self.iBooked ==0:
+                if self.iBooked == 0:
                     print "Sorry !!! You don't have reservations "
                     self.iReserve()
                 print "Legend : 'B' - Booked Seat . 'R' - Reserved Seat"
-                print "You've booked the seat %s" %(self.iBooked)
+                print "You've booked the seat %s" % (self.iBooked)
                 print self.line
-                self.cancel=raw_input("Enter the Cancellation seat :")
-                if int(self.cancel) > 20 or self.srv1[int(self.cancel) -1] !="B":
+                self.cancel = raw_input("Enter the Cancellation seat :")
+                if int(self.cancel) > 20 or self.srv1[int(self.cancel) - 1] != "B":
                     print "Sorry !! Seat cannot be cancelled !!!"
                     print self.line
                     self.iReserve()
                 else:
-                    self.srv1[int(self.cancel) -1 ]=int(self.cancel)
+                    self.srv1[int(self.cancel) - 1] = int(self.cancel)
                 print "Seat Cancelled Successfully !!!"
                 print self.srv1
                 self.HomeMenu()
@@ -142,15 +136,13 @@ class Reserve(object):
                 print "Sorry !!! Login first"
                 self.HomeMenu()
 
-
-
         def iList(self):
             print "List"
 
         print "1. Book : "
         print "2. Cancel: "
         print "3. List : "
-        self.iResMenu=str(raw_input("Enter option : "))
+        self.iResMenu = str(raw_input("Enter option : "))
         if self.iResMenu == "1":
             iBook(self)
         elif self.iResMenu == "2":
@@ -161,11 +153,6 @@ class Reserve(object):
             print "Invalid Option Selected "
             self.iReserve()
 
-
-
-
-
-
 ####### Objects #########
-kickoff=Reserve()
+kickoff = Reserve()
 kickoff.HomeMenu()
